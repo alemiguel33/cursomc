0b22ae51675a8b92bd8189b0d287b4d343cda481
Alexandre de Carvalho Miguel <alemiguel33@gmail.com>
2022-03-16 15:29:59 -0300

    Produto e associacao muitos para muitos

--- src/main/java/com/alexandre/cursomc/domain/Categoria.java
+++ src/main/java/com/alexandre/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package com.alexandre.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -15,13 +18,12 @@ public class Categoria implements Serializable {
 		@Id
 		@GeneratedValue(strategy=GenerationType.IDENTITY)
 		private Integer id;
-	
-	
+		private String nome;
 		
-	private String nome;
+		@ManyToMany(mappedBy="categorias")
+		private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
-		
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -50,6 +52,14 @@ public class Categoria implements Serializable {
 	public int hashCode() {
 		return Objects.hash(id);
 	}
+	
+	public List<Produto> getProdutos(){
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos=produtos;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -64,5 +74,4 @@ public class Categoria implements Serializable {
 	}
 	
 	
-
 }
